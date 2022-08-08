266a1978fbe814098448fb9847722681a392ce18
Carlos Fornari <lilfornah@gmail.com>
2022-08-08 15:17:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Ejecutar en desarrollo
+
+1. Clonar repo
+2. Instalar dependencias
+```
+npm install
+```
+3. Instalar Nest CLI
+```
+npm i -g @nestjs/cli
+```
+4. Levantar db
+```
+docker-compose up -d
+```
+
+## Stack
+* MongoDB
+* NestJS
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,68 @@
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+
+@Injectable()
+export class PokemonService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ){ }
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase().trim();
+    try{
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    }catch(e){
+      this.handleException(e);
+    }
+  }
+
+  async findAll() {
+    return await this.pokemonModel.find();
+  }
+
+  async findOne(searchTerm: string) {
+    let pokemon: Pokemon;
+
+    if(!isNaN(+searchTerm)) 
+      pokemon = await this.pokemonModel.findOne({pokedexNumber: +searchTerm});
+    else if(isValidObjectId(searchTerm))
+      pokemon = await this.pokemonModel.findById(searchTerm);
+    else 
+      pokemon = await this.pokemonModel.findOne({name: searchTerm.toLowerCase().trim()});
+
+    if(!pokemon) throw new NotFoundException(`Pokemon ${searchTerm} not found`);
+    return pokemon;
+  }
+
+  async update(searchTerm: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemon = await this.findOne(searchTerm);
+    if(updatePokemonDto.name) pokemon.name = updatePokemonDto.name.toLowerCase().trim();
+    try{
+      await pokemon.updateOne(updatePokemonDto);
+      return {...pokemon.toJSON(),...updatePokemonDto};
+    }catch(e){
+      this.handleException(e);
+    }
+  }
+
+  async remove(id: string) {
+    const {deletedCount} = await this.pokemonModel.deleteOne({_id: id});
+    if(deletedCount === 0) throw new NotFoundException(`Pokemon with id ${id} not found`);
+  }
+
+  private handleException(e: any) {
+    if(e.code === 11000){
+      throw new BadRequestException(`Pokemon already exists in DB ${JSON.stringify(e.keyValue)}`);
+    }
+    console.log(e)
+    throw new InternalServerErrorException('Error creating pokemon');
+  }
+
+}
